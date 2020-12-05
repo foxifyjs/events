6c42b240746f7f54f346cefe46d1a1332aebe2e0
Ardalan Amini <ardalanamini22@gmail.com>
2020-12-06 00:41:06 +0330

    chore: Improve benchmarks output

--- benchmarks/emit.js
+++ benchmarks/emit.js
@@ -1,3 +1,4 @@
+const { createStream } = require("table");
 const { Suite } = require("benchmark");
 const { EventEmitter } = require("events");
 const { EventEmitter2 } = require("eventemitter2");
@@ -5,6 +6,16 @@ const EventEmitter3 = require("eventemitter3");
 const { EventEmitter: DripEmitter } = require("drip");
 const { EventEmitter: FoxifyEmitterV1 } = require("eventsV1");
 const FoxifyEmitter = require("..").default;
+
+const stream = createStream({
+  columnDefault: {
+    alignment: "center",
+    width: 20,
+  },
+  columnCount: 5,
+});
+
+let base;
 
 function handle() {
   return 1;
@@ -44,9 +55,27 @@ new Suite()
     dripEmitter.emit("foo");
   })
   .on("cycle", (e) => {
-    console.log(e.target.toString());
+    const { name, hz, stats } = e.target;
+
+    if (name === "events") base = hz;
+
+    const size = stats.sample.length;
+
+    stream.write([
+      name,
+      `${hz.toLocaleString("en-US", {
+        maximumFractionDigits: hz < 100 ? 2 : 0,
+      })} ops/sec`,
+      `${(hz / base).toLocaleString("en-US", {
+        maximumFractionDigits: 2,
+      })}X`,
+      `\xb1${stats.rme.toLocaleString("en-US", {
+        maximumFractionDigits: 2,
+      })}%`,
+      `${size} run${size === 1 ? "" : "s"} sampled`,
+    ]);
   })
   .on("complete", function onComplete() {
-    console.log("Fastest is %s", this.filter("fastest").map("name"));
+    console.log("\n> Fastest is %s", this.filter("fastest").map("name"));
   })
   .run();
